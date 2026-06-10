9731fe5677b5f3cdede8dd08ef5c16c7dfa3bb77
theownage <ownage@theownages-MacBook-Pro.local>
2026-06-10 09:18:30 +0200

    added word wrap and line break to config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,9 @@ print("LOADING options.lua")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
